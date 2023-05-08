3ffc1c6edf69f27ca26ec3533216c6a5a2bde682
kenx00x <kenx00x@gmail.com>
2023-05-08 17:13:58 +0200

    new food value system

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,7 +1,30 @@
+using System;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+	public FoodClass FoodClass;
+	[NonSerialized]
 	public float FoodValue = 0;
 	public AudioClip EatingSound;
+
+	void Start()
+	{
+		// Set the food value based on the food class
+		switch (FoodClass)
+		{
+			case FoodClass.DarkGreen:
+				FoodValue = 2;
+				break;
+			case FoodClass.Green:
+				FoodValue = 1f;
+				break;
+			case FoodClass.Orange:
+				FoodValue = 0.5f;
+				break;
+			case FoodClass.Red:
+				FoodValue = -2;
+				break;
+		}
+	}
 }
---
+++ Assets/Scripts/FoodClass.cs
@@ -0,0 +1,7 @@
+public enum FoodClass
+{
+	DarkGreen,
+	Green,
+	Orange,
+	Red
+}
